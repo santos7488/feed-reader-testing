9c9669dd59b7b06e495d9099ec844667b358f092
santos7488 <34498697+santos7488@users.noreply.github.com>
2018-09-08 19:51:59 -0400

    Add files via upload

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -1,19 +1,6 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* All of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-$(() => {
-
-    describe('RSS Feeds', () => {
+describe('RSS Feeds', () => {
         /* Tests to make sure that the allFeeds variable has
-         * been defined and that it is not empty.
-         */
+         * been defined and that it is not empty.*/
         it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -21,8 +8,7 @@ $(() => {
 
         /* Test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+         * and that the URL is not empty.*/
         it('have a valid url', () => {
             allFeeds.forEach((feed) => {
                 expect(feed.url).toBeDefined();
@@ -32,8 +18,7 @@ $(() => {
 
         /* Test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+         * and that the name is not empty.*/
         it('have a valid name', () => {
             allFeeds.forEach((feed) => {
                 expect(feed.name).toBeDefined();
@@ -44,16 +29,15 @@ $(() => {
 
     /* Test suite that checks menu functionality */
     describe('The menu', () => {
+
         /* Test that ensures the menu element is
-         * hidden by default.
-         */
+         * hidden by default.*/
         it('hides the menu by default', () => {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
         /* Test that ensures the menu changes
-         * visibility when the menu icon is clicked.
-         */
+         * visibility when the menu icon is clicked.*/
         it('visibility is toggled upon click on menu icon', () => {
 
             $('.menu-icon-link').click();
@@ -63,22 +47,19 @@ $(() => {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
-});
 
-/* Test suite that checks the loadFeed functionality*/
+    /* Test suite that checks the loadFeed functionality*/
 describe('Initial Entries', () => {
 
     /* Test that ensures when the loadFeed
      * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     */
+     * a single .entry element within the .feed container.*/
     let feedLength;
     beforeEach((done) => {
         loadFeed(0, () => {
             feedLength = $('.feed .entry').length;
             done();
         });
-
     });
 
     it('has at least one entry', (done) => {
@@ -91,8 +72,7 @@ describe('Initial Entries', () => {
 describe('New Feed Selection', () => {
 
     /* Test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     */
+     * by the loadFeed function that the content actually changes.*/
     let feedA,
         feedB;
 
